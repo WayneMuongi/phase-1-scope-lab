169c1cf2efcab716037163ca0ab0c6ca28aad4d0
WayneMuongi <waynemuongi77@gmail.com>
2025-06-17 13:40:36 +0300

    scopes

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName="bob";
+
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer="not bob"
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer="maybe bob"
+}
+
+const leastFavoriteCustomer= "wayne"
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="muongi"
+    return leastFavoriteCustomer;
+}
